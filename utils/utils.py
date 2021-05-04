881cf371316f2f094a95b27835f7914edf72162b
lemswasabi <leminhnguyen@outlook.com>
2021-05-04 11:27:30 +0200

    add tfidf parameters to preprocess_dataset

--- utils/utils.py
+++ utils/utils.py
@@ -29,12 +29,15 @@ def trainset_to_df(path):
 
     return df
 
-def preprocess_dataset(path, text_representation='tfid', feature_selection=None, labels='sentiment'):
+def preprocess_dataset(path, text_representation='tfidf', tfidf_max_features=None, tfidf_min_df=7, tfidf_max_df=0.8, feature_selection=None, labels='sentiment'):
     """
     Preprocess dataset and return features and labels
     Args:
         path: path string to trainset.txt
-        text_representation: representation of text, default tfid
+        text_representation: representation of text, default tfidf
+        tfidf_max_features: max_features for tfidf, default None
+        tfidf_min_df: min_df for tfidf, default 7
+        tfidf_max_df: max_df for tfidf, default 0.8
         feature_selection: type of feature_selection, default None
         labels: return type of labels, default sentiment
     Return:
@@ -64,9 +67,9 @@ def preprocess_dataset(path, text_representation='tfid', feature_selection=None,
     reviews['topic_labels'] = le.inverse_transform(reviews['topic'])
 
     # Vectorize text with Tfid
-    if text_representation == 'tfid':
+    if text_representation == 'tfidf':
 
-        vectorizer = TfidfVectorizer (max_features=2500, min_df=7, max_df=0.8)
+        vectorizer = TfidfVectorizer (max_features=tfidf_max_features, min_df=tfidf_min_df, max_df=tfidf_max_df)
         features = vectorizer.fit_transform(reviews['text']).toarray()
 
     # Get labels
